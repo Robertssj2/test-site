098200f14f848248bafba8d4f992322383a700b0
Robert Woods <ballz_0f_Steel@hotmail.com>
2020-03-04 11:05:42 -0800

    onclick picture changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/fart-face.jpg') {
+        myImage.setAttribute ('src','images/english-muffin.jpg');
+} else {
+    myImage.setAttribute ('src', 'images/fart-face.jpg');
+}
+}
