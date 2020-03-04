90ebf4c8c5c1d4c1b4e2e3fed61b6ae85bb7fe1e
Robert Woods <ballz_0f_Steel@hotmail.com>
2020-03-04 11:21:57 -0800

    added myName button

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,19 @@ myImage.onclick = function() {
     myImage.setAttribute ('src', 'images/fart-face.jpg');
 }
 }
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Great News, ' + myName + '!';
+}
+if(!localStorage.getItem('name ')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Great News, ' + storedName + '!';
+}
+myButton.onclick = function() {
+    setUserName();
+}
